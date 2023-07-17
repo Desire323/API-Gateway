3ca818a09e6357e44d64b70891d948b2ea629b10
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-07-17 21:06:47 +0300

    Upload initial API Gateway

---
+++ src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.apigateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class ApiGatewayApplicationTests {
+
+}
